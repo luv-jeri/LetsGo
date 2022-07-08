ac81abe7b2a8fee95f7039deabac922f30141d9e
luv-jeri <luvvjeri@gmail.com>
2022-07-08 22:00:35 +0530

    Error Hadling

--- app.js
+++ app.js
@@ -1,14 +1,21 @@
 const express = require('express');
 const morgan = require('morgan');
-// # Routes
-const tourRoute = require('./routes/tours.routes');
 
 const app = express();
 
 app.use(morgan('dev'));
 app.use(express.json());
 
-app.use('/api/v1/tours', tourRoute);
+app.use('/api/v1/tours', require('./routes/tours.routes'));
+
+// # Global Error Handling Middleware
+app.use((err, req, res, next) => {
+  const errStatus = err.statusCode || 500;
+  const message = err.message || 'Something went wrong';
+  const status = err.status || 'error';
+
+  res.status(errStatus).json({ message, status });
+});
 
 app.all('*', (req, res) => {
   res.status(404).json({
--- controller/tours.controller.js
+++ controller/tours.controller.js
@@ -1,30 +1,25 @@
 const Tours = require('../database/Schema/tours.schema');
+const catchAsync = require('../utils/catch_async');
+const _Error = require('../utils/_Error');
 
-const addTour = async (req, res) => {
+const addTour = catchAsync(async (req, res, next) => {
   const { name, duration, maxGroupSize, difficulty, price } = req.body;
-  try {
-    const newTour = await Tours.create({
-      name,
-      duration,
-      maxGroupSize,
-      difficulty,
-      price,
-    });
-    res.status(200).json({
-      status: 'success',
-      message: 'Tour added successfully',
-      data: newTour,
-    });
-  } catch (e) {
-    __(e);
-    res.status(400).json({
-      status: 'fail',
-      message: e._message,
-    });
-  }
-};
 
-const getTours = async (req, res) => {
+  const newTour = await Tours.create({
+    name,
+    duration,
+    maxGroupSize,
+    difficulty,
+    price,
+  });
+  res.status(200).json({
+    status: 'success',
+    message: 'Tour added successfully',
+    data: newTour,
+  });
+});
+
+const getTours = catchAsync(async (req, res, next) => {
   let { query } = req; // query is an object
 
   query = JSON.stringify(query); // convert query to string
@@ -35,62 +30,41 @@ const getTours = async (req, res) => {
 
   query = JSON.parse(query); // convert query string to object
 
-  try {
-    const allTours = await Tours.find(query).select({
-      __v: 0,
-    });
+  const allTours = await Tours.find(query).select({
+    __v: 0,
+  });
 
-    res.status(200).json({
-      status: 'success',
-      message: `${allTours.length} tours found`,
-      data: allTours,
-    });
-  } catch (e) {
-    // __(e);
-    res.status(400).json({
-      status: 'fail',
-      message: e._message,
-    });
-  }
-};
+  res.status(200).json({
+    status: 'success',
+    message: `${allTours.length} tours found`,
+    data: allTours,
+  });
+});
 
-const getTourById = async (req, res) => {
-  const { id } = req.params;
-  try {
-    const tour = await Tours.findById(id);
-    res.status(200).json({
-      status: 'success',
-      message: 'Tour found',
-      data: tour,
-    });
-  } catch (e) {
-    res.status(400).json({
-      status: 'fail',
-      message: e._message,
-    });
-  }
-};
-
-const updateTour = async (req, res) => {
+const getTourById = catchAsync(async (req, res) => {
   const { id } = req.params;
 
-  try {
-    const updateTour = await Tours.findByIdAndUpdate(id, req.body, {
-      new: true,
-    });
+  const tour = await Tours.findById(id);
+  res.status(200).json({
+    status: 'success',
+    message: 'Tour found',
+    data: tour,
+  });
+});
 
-    res.status(200).json({
-      status: 'success',
-      message: 'Tour updated successfully',
-      data: updateTour,
-    });
-  } catch (e) {
-    res.status(400).json({
-      status: 'fail',
-      message: e._message,
-    });
-  }
-};
+const updateTour = catchAsync(async (req, res) => {
+  const { id } = req.params;
+
+  const updateTour = await Tours.findByIdAndUpdate(id, req.body, {
+    new: true,
+  });
+
+  res.status(200).json({
+    status: 'success',
+    message: 'Tour updated successfully',
+    data: updateTour,
+  });
+});
 
 module.exports = {
   addTour,
